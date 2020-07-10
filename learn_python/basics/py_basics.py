9107ecb0f476576738bc76c4abecd2d76b66a843
Jitu <jitendrabisht2210@gmail.com>
2020-07-10 21:00:56 +0530

    python basics

--- learn_python/basics/py_basics.py
+++ learn_python/basics/py_basics.py
@@ -9,14 +9,17 @@ cars_not_driven = cars - drivers
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers / cars_driven
-print()
-
 print("There are", cars, "cars available.")
 print("There are only", drivers, "drivers available.")
 print("There will be", cars_not_driven, "empty cars today.")
 print("We can transport", carpool_capacity, "people today.")
 print("We have", passengers, "to carpool today.")
 print("We need to put about", average_passengers_per_car, "in each car.")
+
+print("There are %d types of people." % 10)
+binary = "binary"
+do_not = "don't"
+print("Those who know %s and those who %s." % (binary, do_not))
 
 for i in range(5):
     print(i)
