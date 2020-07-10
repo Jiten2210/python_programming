73939edd02977776ce69ae95f8a6dfa3ac668459
Jitu <jitendrabisht2210@gmail.com>
2020-07-10 18:57:53 +0530

    python basics

--- learn_python/basics/py_basics.py
+++ learn_python/basics/py_basics.py
@@ -1,8 +1,48 @@
 # print
 print("Hi Jitu! Welcome to Python World")
 
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+print()
+
+print("There are", cars, "cars available.")
+print("There are only", drivers, "drivers available.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today.")
+print("We need to put about", average_passengers_per_car, "in each car.")
+
 for i in range(5):
     print(i)
+
+#String
+String1 = "TeamIndia"
+print(String1)
+print(String1[2:7])
+print(String1[3:-2])
+
+# String Formatting
+String1 = "{} {} {}".format('Physics', 'Math', 'Chemistry')
+print(String1)
+
+#positional formatting
+String1 = "{1} {0} {2}".format('Physics', 'Math', 'Chemistry')
+print(String1)
+
+# Keyword Formatting
+String1 = "{p} {c} {m}".format(p='Physics', m='Math', c='Chemistry')
+print(String1)
+
+# Formatting of Integers
+String1 = "{0:b}".format(16)
+print("\nBinary representation of 16 is ")
+print(String1) 
 
 # List
 a = [11, 21, 13, 4, 54, 36]
