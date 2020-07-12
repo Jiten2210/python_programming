4f6c47df9d78a0a1685f7fab2cbb94d9951e297c
Jitu <jitendrabisht2210@gmail.com>
2020-07-12 22:58:59 +0530

    pyhton basics

--- learn_python/basics/program.py
+++ learn_python/basics/program.py
@@ -1,7 +1,7 @@
 # reverse a list
-list = [10, 20, 30, 40, 50]
-list = list[::-1]
-print(list)
+aList = [10, 20, 30, 40, 50]
+aList = aList[::-1]
+print(aList)
 
 # palindrome check
 x = "madam"
@@ -30,7 +30,7 @@ index = list5.index(11)
 list5[index] = 4
 print(list5)
 
-#lowercase letter first
+# lowercase letter first
 
 inputStr = "PyThoN"
 words = inputStr.split()
@@ -57,21 +57,21 @@ print("gaya occured", count, "times")
 input1 = "indiaisagreatnation"
 dict = dict()
 for char in input1:
-  count = input1.count(char)
-  dict[char]=count
+    count = input1.count(char)
+    dict[char] = count
 print(dict)
 
 #  Access the value of key ‘history’
 sampleDict = {
-   "class":{
-      "student":{
-         "name":"Mike",
-         "marks":{
-            "physics":70,
-            "history":80
-         }
-      }
-   }
+    "class": {
+        "student": {
+            "name": "Mike",
+            "marks": {
+                "physics": 70,
+                "history": 80
+            }
+        }
+    }
 }
 
 print(sampleDict['class']['student']['marks']['history'])
@@ -79,9 +79,9 @@ print(sampleDict['class']['student']['marks']['history'])
 # Create new dictionary with given key
 
 inputDict = {
-  "name": "MSD",
-  "age":39,
-  "city": "Ranchi" }
+    "name": "MSD",
+    "age": 39,
+    "city": "Ranchi"}
 
 keys = ["name", "city"]
 
@@ -90,10 +90,60 @@ print(newDict)
 
 # swap numbers
 
-x=10
-y=20
-x,y = y,x
+x = 10
+y = 20
+x, y = y, x
 print(x)
 print(y)
 
+# check it all elements are same
 
+tuple1 = (1, 1, 1, 1)
+print(all(i == tuple1[0] for i in tuple1))
+
+
+# variable arguments
+
+def func(*args):
+    for i in args:
+        print(i)
+
+
+func(20, 40, 60)
+func(80, 100)
+
+
+# default param
+def getEmployee(name, salary=9000):
+    print("Employee", name, "salary is:", salary)
+
+
+getEmployee("X", 80000)
+getEmployee("Y")
+
+# even numbers between 1 and 20
+
+print(list(range(0, 20, 2)))
+
+# sort words alphabetically
+
+str = "India is a great nation"
+words = str.split()
+
+words.sort()
+for word in words:
+    print(word)
+
+# count number of vowels
+#method 1
+vowels = 'aeiou'
+str = str.casefold()
+count = {}.fromkeys(vowels, 0)
+
+for c in str:
+    if c in count:
+        count[c]+=1
+print(count)
+
+#method 2
+print({x:sum([1 for char in str if char == x]) for x in vowels})
